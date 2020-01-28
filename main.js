49029885fee2a02887cb70d013b774db203f7f89
TheMightyQuinn6744 <quinn@rockitscienceagency.com>
2020-01-28 15:26:50 -0600

    Fixed scrolling navbar

--- main.js
+++ main.js
@@ -28,26 +28,38 @@ window.onload=function() {
 window.onscroll = function() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop < 100) {
         document.getElementById("navbar").style.top = "0";
+        //hides content of scrolling navbar 
         document.getElementById("navtext1").style.display = "none";
         document.getElementById("navtext2").style.display = "none";
         document.getElementById("navtext3").style.display = "none";
+        //applies transparent background to relative navbar
         document.getElementById("navbar").style.backgroundColor = "transparent";
+        //display contents of relative navbar
         document.getElementById("navtextA").style.display = "";
         document.getElementById("navtextB").style.display = "";
         document.getElementById("navtextC").style.display = "";
+        //hides hidden beauty logo image contained in scrolling navbar
         document.getElementById("navlogo").style.display="none";
 
     } else {
+        //hides scrolling navbar until user scrolls 100px
         document.getElementById("navbar").style.top = "-50px;";
-        document.getElementById("navbar").style.top = "-100px;";
+        //reveals hidden beauty logo to scrolling navbar beyond 100px
         document.getElementById("navlogo").style.display="";
+        //reveals contents of scrolling navbar beyond 100px
         document.getElementById("navtext1").style.display = "";
         document.getElementById("navtext2").style.display = "";
         document.getElementById("navtext3").style.display = "";
+        //applies white background color to scrolling navbar beyond 100px
         document.getElementById("navbar").style.backgroundColor = "white";
+        //hides content from relative navbar
         document.getElementById("navtextA").style.display = "none";
         document.getElementById("navtextB").style.display = "none";
         document.getElementById("navtextC").style.display = "none";
+
+        
+        document.getElementById("navlogo").style.display = "block";
+        document.getElementById("navscroll").style.display = "";
 
     }
 };
