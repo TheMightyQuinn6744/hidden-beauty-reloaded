f7ee9626fb3fdcfbf82a3f78c36d6a504afbe207
TheMightyQuinn6744 <quinn@rockitscienceagency.com>
2020-01-28 12:55:27 -0600

    Fixed navigation bar for scrolling navbar, and relative navbar. Fixed div structuring.

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 //assign varibale to button assignments for cut/style & color 
 let cutButton = document.getElementById('styleButton');
 let colorButton = document.getElementById('colorButton');
+
 //assign variable to table assignments for table1(cut/style) & table2(color)
 const cutTable = document.getElementById('table1');
 const colorTable = document.getElementById('table2');
+
 //write function for cut/style on click event 
 cutButton.onclick = function() {
     cutTable.style.display='';
     colorTable.style.display='none';
 };
+
 //write function for color style on click event 
 colorButton.onclick = function() {
     colorTable.style.display='';
     cutTable.style.display='none';
-    
+
 };
-//function that clicks styleButton by default upon page loading
+
+//window load default click for cut/style
 window.onload=function() {
     document.getElementById("styleButton").click();
-};
-//When the user scrolls down 20px from the top of the document, slide down the navbar 
-//When the user scrolls to the top of the page, slide up the navbar (50px out of the top of the view)
+}; 
+
+
 window.onscroll = function() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop < 100) {
         document.getElementById("navbar").style.top = "0";
@@ -32,8 +36,9 @@ window.onscroll = function() {
         document.getElementById("navtextB").style.display = "";
         document.getElementById("navtextC").style.display = "";
         document.getElementById("navlogo").style.display="none";
-        
+
     } else {
+        document.getElementById("navbar").style.top = "-50px;";
         document.getElementById("navbar").style.top = "-100px;";
         document.getElementById("navlogo").style.display="";
         document.getElementById("navtext1").style.display = "";
@@ -46,4 +51,3 @@ window.onscroll = function() {
 
     }
 };
-
